d6bfd9e189d1dcfc962e38f1e8c857b9f076c486
diaokeshijian <ybdavid@163.com>
2020-09-20 21:23:23 +0800

    add more code on batch download for t66y.

--- src/autoweb/autot66ytest.py
+++ src/autoweb/autot66ytest.py
@@ -1,7 +1,11 @@
+import os
+
 import requests
 
+article_urls = []
 img_urls = []
 counter = 0
+root_path = 'C:\\Users\\Eli\\Desktop\\test\\'
 headers = {
     'Accept': '*/*',
     'Accept-Encoding': 'gbk, utf-8',
@@ -9,23 +13,20 @@ headers = {
     'Connection': 'keep-alive',
     'host': 't66y.com',
     'Referer': 'https://t66y.com/',
-    'User-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW 64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36 QIHU 360SE'
+    'User-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW 64) AppleWebKit/537.36 '
+                  '(KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36 QIHU 360SE'
 }
-
-
-def get_title(response_content):
-    content = response_content.split('title')
-    print(content)
-
-
-response = requests.get('https://t66y.com/htm_data/2009/16/4089558.html', headers=headers)
+response = requests.get('https://t66y.com/thread0806.php?fid=16', headers=headers)
 print(response.status_code)
+
 response.encoding = 'gbk'
 response_text = response.text
-title = response_text.split('<title>')[1].split('</title>')[0].replace('&nbsp;', '')
-if ' - 達蓋爾的旗幟 | 草榴社區 - t66y.com' in title:
-    title = title.replace(' - 達蓋爾的旗幟 | 草榴社區 - t66y.com', '')
-print(title)
+# print(response_text)
 
-
+response_text_in_lines = response_text.split('h3>')
+for line in response_text_in_lines:
+    if 'target="_blank"' in line and 'stylesheet' not in line and '永久域名' not in line and 'color=blue' not in line\
+            and 'color=red' not in line and 'color=orange' not in line:
+        article_url = line.split('"')[1]
+        print('https://t66y.com/' + article_url)
 
