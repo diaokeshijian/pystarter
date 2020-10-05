e73bdff90a2e74c90e29164244ade3b282521b3f
diaokeshijian <ybdavid@163.com>
2020-10-06 00:39:42 +0800

    add more code on batch download for t66y.

--- src/autoweb/verify.py
+++ src/autoweb/verify.py
@@ -1,9 +1,18 @@
 import requests
 
+headers = {
+    'Accept': '*/*',
+    'Accept-Encoding': 'gbk, utf-8',
+    'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+    'Connection': 'keep-alive',
+    'host': 't66y.com',
+    'Referer': 'https://t66y.com/',
+    'User-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW 64) AppleWebKit/537.36 '
+                  '(KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36 QIHU 360SE'
+}
 index_urls = []
 article_urls = []
 counter = 0
-headers = ""
 root_path = 'C:\\temp\\image_downloader\\'
 image_directory = "C:\\temp\\image_downloader\\downloaded_images\\"
 config_directory = "C:\\temp\\image_downloader\\config\\"
@@ -12,6 +21,7 @@ config_directory = "C:\\temp\\image_downloader\\config\\"
 def main():
     get_index_urls()
     get_article_urls()
+    print(article_urls)
 
 
 def get_index_urls():
@@ -33,7 +43,8 @@ def get_article_urls():
                     and 'color=red' not in line and 'color=orange' not in line:
                 article_url_short = line.split('"')[1]
                 article_url_long = 'https://t66y.com/' + article_url_short
-                article_urls.append(article_url_long)
+                if article_url_long not in article_urls:
+                    article_urls.append(article_url_long)
 
 
 def get_request_header():
@@ -46,3 +57,4 @@ def get_request_header():
             headers = headers + line
 
 
+main()
